628011beaa7bb01a01abb926595c7e13f48d0d4f
Sariya <zhoki@users.noreply.github.com>
2015-03-26 21:30:59 +0700

    Fix project,doc,doctemplate model

--- pdms/app/models/project.rb
+++ pdms/app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
 has_and_belongs_to_many :users
+has_many :documents
+has_many :tasks
 has_many :project_users, dependent: :destroy
 end
--- pdms/app/models/task.rb
+++ pdms/app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-  belongs_to :document
+  belongs_to :document_template
   belongs_to :approver, :class_name => "User", :foreign_key => "approver_id"
   belongs_to :assignee, :class_name => "User", :foreign_key => "assignee_id"
 end
